5e210587ec54845a637c1676b300a4c5d9a272aa
new1333 <dearest@abouttime.cn>
2021-02-04 22:57:35 +0800

    update watcher

--- src/main.js
+++ src/main.js
@@ -2,42 +2,42 @@ const babelParser = require("@babel/parser");
 const babelTraverse = require("@babel/traverse").default;
 
 const code = `
-  function sum(a, b) {
-    var result = 0 
-    result = a + b
-    return result 
-  }
+function sum(aaa, b) {
+  var result = 0  
+  result = a + b 
+  return result 
+}
 `;
 
-const ast = babelParser.parse(code);
+function compile() {
+  console.log("compile");
+  const ast = babelParser.parse(code);
 
-let a16Codes = [];
+  let a16Codes = [];
 
-function push(str) {
-  a16Codes.push(str);
+  function push(str) {
+    a16Codes.push(str);
+  }
+
+  babelTraverse(ast, {
+    enter(path) {
+      if (path.isFunctionDeclaration()) {
+        const node = path.node;
+        const {
+          params,
+          id: { name: functionName },
+        } = node;
+        push(`@${functionName}`);
+        params.forEach((param) => {
+          push(`.var2${param.name}`);
+        });
+      }
+      if (path.isVariableDeclaration()) {
+        const node = path.node;
+      }
+    },
+  });
+  return a16Codes.join("\n");
 }
 
-babelTraverse(ast, {
-  enter(path) {
-    if (path.isFunctionDeclaration()) {
-      const node = path.node;
-      const {
-        params,
-        id: { name: functionName },
-      } = node;
-      push(`@${functionName}`);
-      params.forEach((param) => {
-        push(`.var2 ${param.name}`);
-      });
-    }
-    if (path.isVariableDeclaration()) {
-      const node = path.node;
-      const {
-        params,
-        id: { name: varName },
-      } = node;
-    }
-  },
-});
-
-console.log(a16Codes);
+module.exports = compile;
--- src/watcher.js
+++ src/watcher.js
@@ -5,6 +5,17 @@ const inputFile = "./main.js";
 const output = "./code.a16";
 
 let count = 0;
-fs.watchFile(path.resolve(__dirname, inputFile), { interval: 1000 }, () => {
-  fs.writeFileSync(path.resolve(__dirname, output), count++);
+
+function requireUncached(module) {
+  delete require.cache[require.resolve(module)];
+  return require(module);
+}
+
+fs.watch(path.resolve(__dirname, inputFile), { interval: 1000 }, () => {
+  try {
+    fs.writeFileSync(
+      path.resolve(__dirname, output),
+      requireUncached("./main.js")()
+    );
+  } catch (error) {}
 });
