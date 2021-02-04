7803b2e9645f34e5fa8d3b641f82974e1b46a78a
new1333 <dearest@abouttime.cn>
2021-02-04 23:28:35 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 const babelParser = require("@babel/parser");
 const babelTraverse = require("@babel/traverse").default;
 
-const code = `
-function sum(aaa, b) {
-  var result = 0  
-  result = a + b 
-  return result 
-}
-`;
-
 function compile() {
+  const code = `
+
+    function sum(a, b) {
+      var result 
+      result = a + b 
+      return result  
+    }
+
+    function main() {
+      var x = 5
+      var y = 10
+      sum(x, y)
+    }
+  `;
   console.log("compile");
   const ast = babelParser.parse(code);
 
@@ -21,19 +27,35 @@ function compile() {
 
   babelTraverse(ast, {
     enter(path) {
+      const node = path.node;
+
       if (path.isFunctionDeclaration()) {
-        const node = path.node;
         const {
           params,
           id: { name: functionName },
         } = node;
         push(`@${functionName}`);
         params.forEach((param) => {
-          push(`.var2${param.name}`);
+          push(`.var2 ${param.name}`);
         });
       }
-      if (path.isVariableDeclaration()) {
-        const node = path.node;
+      if (path.isVariableDeclarator()) {
+        const { id, init } = node;
+        const varName = id.name;
+        const varValue = init && init.value;
+        if (varValue != null) {
+          push(`.var2 ${varName} ${varValue}`);
+        } else {
+          push(`.var2 ${varName} `);
+        }
+      }
+      if (path.isAssignmentExpression()) {
+        const destName = node.left.name;
+        const varName1 = node.right.left.name;
+        const varName2 = node.right.right.name;
+        const op = node.right.operator;
+
+        console.log({ destName, varName1, varName2, op });
       }
     },
   });
